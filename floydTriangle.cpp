6ab96a8cb0ab4bbe2a2724284d6fd0da3e753dee
Huma Suhaib <humasuhaib786@gmail.com>
2024-02-07 11:51:32 +0530

    floydTriangle.cpp
    Code gives output as pattern of  triangle with increasing number ( Floyd's triangle.)

---
+++ floydTriangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+// Here we will write number of rows and increasing numbers pattern(Floyd triangle).
+
+int main(){
+int n;
+cout<<"Enter the numbers here: ";
+cin>>n;
+int sum=0;
+
+for(int i=1;i<=n;i++){
+for(int j=1;j<=i;j++){
+    sum=sum+1;
+    cout<<sum<<" ";
+}
+
+cout<<endl;
+}
